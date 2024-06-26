d9745821a6eff284430ed544dba4e0e0c83356a5
countenum <countenum404@gmail.com>
2024-06-26 16:01:35 +0300

    Added struct player

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"shabashov/tic-tac-toe/internal/board"
+	"shabashov/tic-tac-toe/internal/player"
 )
 
 /*
@@ -15,28 +16,28 @@ import (
 func main() {
 	fmt.Println("Tic-Tac-Toe!")
 	// select a player
-	player1 := "X"
-	player2 := "O"
+	player1 := player.Player{Name: "Player1", Mark: "X"}
+	player2 := player.Player{Name: "Player2", Mark: "O"}
 	board := board.Board{}
 	board.Init()
 	var x1, y1, x2, y2 int
 	for {
 		board.Print()
-		fmt.Println("Try", player1)
+		fmt.Println("Try", player1.Name)
 		fmt.Scan(&x1, &y1)
-		board.SetCell(x1, y1, player1)
+		board.SetCell(x1, y1, player1.Mark)
 
-		if board.CheckWinnerIs(player1) {
+		if board.CheckWinnerIs(player1.Mark) {
 			println("player1 is WINNER!!!")
 			break
 		}
 
 		board.Print()
-		fmt.Println("Try", player2)
+		fmt.Println("Try", player2.Name)
 		fmt.Scan(&x2, &y2)
-		board.SetCell(x2, y2, player2)
+		board.SetCell(x2, y2, player2.Mark)
 
-		if board.CheckWinnerIs(player2) {
+		if board.CheckWinnerIs(player2.Mark) {
 			println("player2 is WINNER!!!")
 			break
 		}
---
+++ internal/player/player.go
@@ -0,0 +1,6 @@
+package player
+
+type Player struct {
+	Name string
+	Mark string
+}
